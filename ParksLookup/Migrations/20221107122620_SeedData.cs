b5eae472fd8becdb68698e1c05ac5829d8151ea1
TrevorH08 <trevorh8700@gmail.com>
2022-11-07 04:28:03 -0800

    add SeedData Migration

---
+++ ParksLookup/Migrations/20221107122620_SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksLookup.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Name", "PointsOfInterest", "StateOrNational" },
+                values: new object[] { 1, "Yellowstone", "Steam Boat Geyser, Old Faithful Geyser Mammoth Hotsprings", "National" });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Name", "PointsOfInterest", "StateOrNational" },
+                values: new object[] { 2, "Yosemite", "Half Dome, Glacier Point, Yosemite Falls", "National" });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Name", "PointsOfInterest", "StateOrNational" },
+                values: new object[] { 3, "Hearst San Simeon", "Hearst Castle, San Simeon Pier, San Simeon Camp Grounds", "State" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3);
+        }
+    }
+}
--- ParksLookup/Models/ParksLookupContext.cs
+++ ParksLookup/Models/ParksLookupContext.cs
@@ -10,5 +10,15 @@ namespace ParksLookup.Models
       }
 
       public DbSet<Park> Parks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Park>()
+        .HasData(
+          new Park { ParkId = 1, Name = "Yellowstone", StateOrNational = "National", PointsOfInterest = "Steam Boat Geyser, Old Faithful Geyser Mammoth Hotsprings"},
+          new Park { ParkId = 2, Name = "Yosemite", StateOrNational = "National", PointsOfInterest = "Half Dome, Glacier Point, Yosemite Falls"},
+          new Park { ParkId = 3, Name = "Hearst San Simeon", StateOrNational = "State", PointsOfInterest = "Hearst Castle, San Simeon Pier, San Simeon Camp Grounds"}
+        );
+    }
   }
 }
