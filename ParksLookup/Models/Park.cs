479e85990855469ddf30df2beca16e3df16a732a
TrevorH08 <trevorh8700@gmail.com>
2022-11-07 06:10:06 -0800

    add validations to model, more reading on authorization

--- ParksLookup/Models/Park.cs
+++ ParksLookup/Models/Park.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ParksLookup.Models
 {
   public class Park
   {
     public int ParkId { get; set; }
+    [Required]
+    [StringLength(25, ErrorMessage = "Name must be less that 25 characters, try cutting it down if it's too long")]
     public string Name { get; set; }
+    [Required]
     public string StateOrNational { get; set; }
     public string PointsOfInterest { get; set; }
   }
